2084a2c0aefc6a268bd36831f74a776b1e23f449
Savelev Rostislav <srv.spb@yandex.ru>
2023-04-18 23:41:23 +0300

    add: у бота добавлены новые команды

--- bot_telegram.py
+++ bot_telegram.py
@@ -18,15 +18,45 @@ import os
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
+'''*********************КЛИЕНТСКАЯ ЧАСТЬ*********************'''
+
+
+async def on_startup(_):
+    print('Бот вышел в онлайн')
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/PizzaAssistantBot')
+
+
+@dp.message_handler(commands=['Режим_работы'])
+async def pizza_open_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+
+
+@dp.message_handler(commands=['Расположение'])
+async def pizza_place_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'ул. Колбасная д. 15')
+
+
+'''*********************АДМИНСКАЯ ЧАСТЬ**********************'''
+
+'''*********************ОБЩАЯ ЧАСТЬ**************************'''
+
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    # await message.answer(message.text)
+    await message.answer(message.text)
     # await message.reply(message.text)
     # бот отправляет сообщение в ЛС пользователя, но только если пользователь активировал этого бота
     # иначе вылетит исключение
-    await bot.send_message(message.from_user.id, message.text)
+    # await bot.send_message(message.from_user.id, message.text)
 
 
 # skip_updates - паараметр для игнорирования запросов поступивших боту, когда он был офлайн
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
